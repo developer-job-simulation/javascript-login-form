7f695827349ebbb3023b6ca400c49ca0af765fa0
Jason Burgess <jason.nzd@gmail.com>
2022-11-11 12:23:47 +1300

    fixed logo, input validation, css center, login

--- src/script.js
+++ src/script.js
@@ -30,4 +30,13 @@ addEventListener("submit", (event) => {
     2. If they are, call renderSuccess()
     3. If they are not, call renderError()
    */
+
+    let validLogin = false;
+    usersTable.forEach((user) =>
+    {
+      if (user.username === email && user.password === password) validLogin = true;
+    });
+
+    validLogin ? renderSuccess() : renderError();
+
 });
