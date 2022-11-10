912428970f7609da19cb9898614a90a11c342d56
Zishan <zishansiddique80@gmail.com>
2022-11-10 21:29:42 +0545

    validation message issue solved

--- src/script.js
+++ src/script.js
@@ -24,10 +24,14 @@ addEventListener("submit", (event) => {
 
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
-  /*
-    TODO:
-    1. Check if the email and password are valid (using the usersTable)
-    2. If they are, call renderSuccess()
-    3. If they are not, call renderError()
-   */
+
+  if (
+    usersTable.find(
+      (element) => element.username === email && element.password === password
+    )
+  ) {
+    renderSuccess();
+  } else {
+    renderError();
+  }
 });
