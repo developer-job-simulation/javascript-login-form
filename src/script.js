edab454820b205d38a4957788eed5a459ba15b88
Chris <chris@getfit.us>
2022-11-11 14:27:10 -0500

    fixed

--- src/script.js
+++ src/script.js
@@ -2,7 +2,7 @@ const usersTable = [
   // Note: This is a fake table for educational purposes. Never store user credentials in plain text.
   { id: 1, username: "hello@world.com", password: "badpassword" },
   { id: 2, username: "test@user.com", password: "badpassword" },
-  { id: 3, username: "email@domain.com", password: "badpassword" },
+  { id: 3, username: "  ", password: "badpassword" },
 ];
 let renderSuccess = () => {
   document.getElementById("success-message").hidden = false;
