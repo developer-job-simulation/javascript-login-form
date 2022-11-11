2257611f38179ce64ac4072653f7023ec451020a
Michael Harris <46806666+MichaelHarrisCode@users.noreply.github.com>
2022-11-10 22:42:01 -0800

    Validate user credentials

--- src/script.js
+++ src/script.js
@@ -24,10 +24,22 @@ addEventListener("submit", (event) => {
 
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
+
   /*
     TODO:
     1. Check if the email and password are valid (using the usersTable)
     2. If they are, call renderSuccess()
     3. If they are not, call renderError()
    */
+  let success = false;
+  usersTable.forEach((curr) => {
+    if (email == curr["username"] && password == curr["password"]) {
+      success = true;
+    }
+  });
+
+  if (success)
+    renderSuccess();
+  else
+    renderError();
 });
