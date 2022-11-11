44d207588364394192a74c5c10d938a8aad01d74
Peter Lawson <lawsonpd@pm.me>
2022-11-10 22:18:10 -0600

    FIX user auth.

--- src/script.js
+++ src/script.js
@@ -30,17 +30,14 @@ addEventListener("submit", (event) => {
     2. If they are, call renderSuccess()
     3. If they are not, call renderError()
    */
-  let success = false;
-  usersTable.forEach((user) => {
-    if (user.username == email) {
-      if (user.password == password) {
-	renderSuccess()
-	success = true;
-      }
-    }
-  })
-  if (!success) {
-      renderError()
+  let validUser = usersTable.find(user =>
+      user.username === email && user.password === password
+  );
+
+  if (validUser) {
+      renderSuccess();
+  } else {
+      renderError();
   }
 });
 
