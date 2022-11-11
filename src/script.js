5f7c1f50b15d45b485dd62187aab0a7d7b57af4d
Jacob Andersen <jacob@algorithmjunkie.com>
2022-11-10 21:57:48 -0800

    add user credential validation and show validation status message, closes #6

--- src/script.js
+++ src/script.js
@@ -24,10 +24,11 @@ addEventListener("submit", (event) => {
 
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
-  /*
-    TODO:
-    1. Check if the email and password are valid (using the usersTable)
-    2. If they are, call renderSuccess()
-    3. If they are not, call renderError()
-   */
+  
+  let user = usersTable.find(entry => entry.username === email);
+  if (!user) {
+    return renderError();
+  }
+
+  return user.password === password ? renderSuccess() : renderError();
 });
