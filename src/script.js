ab7ac24f69c43d47dc37a5f26fa9573dbdc2c003
galmodovar <galmodovar1@gmail.com>
2022-11-13 19:36:34 -0600

    fix

--- src/script.js
+++ src/script.js
@@ -34,5 +34,5 @@ addEventListener("submit", (event) => {
     let valid = usersTable.map((user) => {
       return email === user.username && password === user.password
     })
-    return valid.includes(true) ? renderSuccess() : renderError() 
+    return valid.find(true) ? renderSuccess() : renderError() 
 });
