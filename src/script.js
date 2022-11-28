26375dc56f56bc73a04f460c2c349d78b98dc664
Sidharth Mahala <sidharthmahala@gmail.com>
2022-11-28 20:08:49 +0530

    strictly equal

--- src/script.js
+++ src/script.js
@@ -21,12 +21,13 @@ addEventListener("submit", (event) => {
 
   let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
+  
   const check=usersTable.find(e=>{
-    return e.username === email && e.password === password;
+    return e.username == email && e.password == password;
   })
   
   check ? renderSuccess(): renderError();
-  
+
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
   /*
