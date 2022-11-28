68f138c5d9ea94b6a53f92e4dbd322345d09c8a1
KathidB <Kathidbb@gmail.com>
2022-11-28 21:57:02 +0100

    fixed five issues

--- src/script.js
+++ src/script.js
@@ -24,10 +24,19 @@ addEventListener("submit", (event) => {
 
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
+
   /*
     TODO:
     1. Check if the email and password are valid (using the usersTable)
     2. If they are, call renderSuccess()
     3. If they are not, call renderError()
    */
+
+  for (let i = 0; i < usersTable.length; i++) {
+    if (email == usersTable[i].username && password == usersTable[i].password) {
+      renderSuccess();
+    } else {
+      renderError();
+    }
+  }
 });
