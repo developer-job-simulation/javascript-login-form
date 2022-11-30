4b35639a21eb588739cc9f6f042dcda818c97c98
Jared Mercer <jaredthebackpacker@gmail.com>
2022-11-30 10:25:24 -0800

    Added credential validation and validation status message

--- src/script.js
+++ src/script.js
@@ -1,33 +1,31 @@
 const usersTable = [
-  // Note: This is a fake table for educational purposes. Never store user credentials in plain text.
-  { id: 1, username: "hello@world.com", password: "badpassword" },
-  { id: 2, username: "test@user.com", password: "badpassword" },
-  { id: 3, username: "email@domain.com", password: "badpassword" },
+    // Note: This is a fake table for educational purposes. Never store user credentials in plain text.
+    { id: 1, username: "hello@world.com", password: "badpassword" },
+    { id: 2, username: "test@user.com", password: "badpassword" },
+    { id: 3, username: "email@domain.com", password: "badpassword" },
 ];
 let renderSuccess = () => {
-  document.getElementById("success-message").hidden = false;
+    document.getElementById("success-message").hidden = false;
 };
 let renderError = () => {
-  document.getElementById("error-message").hidden = false;
+    document.getElementById("error-message").hidden = false;
 };
 let resetMessage = () => {
-  document.getElementById("success-message").hidden = true;
-  document.getElementById("error-message").hidden = true;
+    document.getElementById("success-message").hidden = true;
+    document.getElementById("error-message").hidden = true;
 };
 
 addEventListener("submit", (event) => {
-  event.preventDefault();
-  resetMessage();
+    event.preventDefault();
+    resetMessage();
 
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    let auth = false;
 
-  console.log(`email submitted: ${email}`);
-  console.log(`password submitted: ${password}`);
-  /*
-    TODO:
-    1. Check if the email and password are valid (using the usersTable)
-    2. If they are, call renderSuccess()
-    3. If they are not, call renderError()
-   */
+    for (let i = 0; i < usersTable.length; i++) {
+        if (usersTable[i].username === email && usersTable[i].password === password) auth = true;
+    }
+
+    auth ? renderSuccess() : renderError();
 });
