0a952d10eaed6d9d04e4b29287c62366bbc6bb71
I-Maged <magedhanafy@gmail.com>
2022-12-16 13:46:22 +0200

    fix

--- src/script.js
+++ src/script.js
@@ -1,26 +1,26 @@
 const usersTable = [
   // Note: This is a fake table for educational purposes. Never store user credentials in plain text.
-  { id: 1, username: "hello@world.com", password: "badpassword" },
-  { id: 2, username: "test@user.com", password: "badpassword" },
-  { id: 3, username: "email@domain.com", password: "badpassword" },
+  { id: 1, username: 'hello@world.com', password: 'badpassword' },
+  { id: 2, username: 'test@user.com', password: 'badpassword' },
+  { id: 3, username: 'email@domain.com', password: 'badpassword' },
 ];
 let renderSuccess = () => {
-  document.getElementById("success-message").hidden = false;
+  document.getElementById('success-message').hidden = false;
 };
 let renderError = () => {
-  document.getElementById("error-message").hidden = false;
+  document.getElementById('error-message').hidden = false;
 };
 let resetMessage = () => {
-  document.getElementById("success-message").hidden = true;
-  document.getElementById("error-message").hidden = true;
+  document.getElementById('success-message').hidden = true;
+  document.getElementById('error-message').hidden = true;
 };
 
-addEventListener("submit", (event) => {
+addEventListener('submit', (event) => {
   event.preventDefault();
   resetMessage();
 
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
 
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
@@ -30,4 +30,14 @@ addEventListener("submit", (event) => {
     2. If they are, call renderSuccess()
     3. If they are not, call renderError()
    */
+
+  let validation = null;
+
+  usersTable.forEach((user) => {
+    if (email === user.username && password === user.password) {
+      validation = true;
+    }
+  });
+
+  return validation ? renderSuccess() : renderError();
 });
