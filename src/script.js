7339fe2180cb96479ad3423fffe93794d38c0bb6
sean <custodiocsean@gmail.com>
2023-02-23 11:06:55 +0800

    validate email and password

--- src/script.js
+++ src/script.js
@@ -25,6 +25,12 @@ addEventListener("submit", (event) => {
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
 
+  /*
+    TODO:
+    1. Check if the email and password are valid (using the usersTable)
+    2. If they are, call renderSuccess()
+    3. If they are not, call renderError()
+   */
   const isValid = usersTable.find((val) => {
     return val.username == email && val.password == password;
   });
@@ -34,10 +40,4 @@ addEventListener("submit", (event) => {
   } else {
     renderError();
   }
-  /*
-    TODO:
-    1. Check if the email and password are valid (using the usersTable)
-    2. If they are, call renderSuccess()
-    3. If they are not, call renderError()
-   */
 });
