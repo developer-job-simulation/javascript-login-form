5d4ad96b8b8a343658f31c178171f08533af8fdc
Josh <hashimotojosh@gmail.com>
2023-03-31 15:22:26 -0700

    fixed all bugs

--- src/script.js
+++ src/script.js
@@ -24,6 +24,20 @@ addEventListener("submit", (event) => {
 
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
+
+  let validCredentials = false; //initially set to false, and will be set to true if the email and password match any of the objects in the `usersTable` array.
+
+  usersTable.forEach(user => {
+    if (email === user.username && password === user.password) {
+      validCredentials = true;
+    }
+  }); //this iterates over the `usersTable` array, and checks if the email and password match any of the objects in the array. If there is at least one match, the `validCredentials` variable is set to `true`.
+
+  if (validCredentials) {
+    renderSuccess();
+  } else {
+    renderError();
+  }; //this checks if the `validCredentials` variable is set to `true`. If it is, it calls the `renderSuccess()` function. If it is not, it calls the `renderError()` function.
   /*
     TODO:
     1. Check if the email and password are valid (using the usersTable)
