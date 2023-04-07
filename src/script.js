b42f09b06487d64acd2e836aaf929f33dc6e3ffd
moejo-the-jojo <108475765+moejo-the-jojo@users.noreply.github.com>
2023-04-07 22:40:27 +0200

    fixed all commented bugs and stuff

--- src/script.js
+++ src/script.js
@@ -25,13 +25,18 @@ addEventListener("submit", (event) => {
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
 
-  
+  const currUser = usersTable.filter((a) => a.username == email);
+  if (currUser.password == password) {
+    renderSuccess();
+  } else {
+    renderError();
+  }
 
   /*
+    DONE
     TODO:
     1. Check if the email and password are valid (using the usersTable)
     2. If they are, call renderSuccess()
     3. If they are not, call renderError()
    */
-
 });
