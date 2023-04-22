d88b29835a5a4635bac05707ef141500e0aa6ffd
kelly <kelly.tate.dev@gmail.com>
2023-04-22 12:55:08 -0700

    Centers login form on page.

--- src/script.js
+++ src/script.js
@@ -25,7 +25,7 @@ addEventListener("submit", (event) => {
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
   /*
-    TODO:
+    DONE:
     1. Check if the email and password are valid (using the usersTable)
     2. If they are, call renderSuccess()
     3. If they are not, call renderError()
