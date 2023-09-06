18d2438220f9e59e7ceac20e10044d21487d1edf
dazfz <dazhi891@gmail.com>
2023-09-06 13:36:25 -0300

    fix logo, center form, email and password type and user validation

--- src/script.js
+++ src/script.js
@@ -24,10 +24,8 @@ addEventListener("submit", (event) => {
 
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
-  /*
-    TODO:
-    1. Check if the email and password are valid (using the usersTable)
-    2. If they are, call renderSuccess()
-    3. If they are not, call renderError()
-   */
+
+  const user = usersTable.find((user) => user.username === email && user.password === password);
+
+  user? renderSuccess(): renderError();
 });
