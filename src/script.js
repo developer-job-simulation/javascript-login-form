942431359130ca03210e0397ff43d70181354ff0
Jared Rairdon <jaredsrairdon@gmail.com>
2023-09-06 12:38:59 -0500

    Fixed all TODO tasks:
    Fix Logo Rendering Issue
    Add HTML Email Validation to Email Input
    Update HTML Password Input to Hide Password Visibility
    Adjust CSS so that form is centered on page
    Validate User Credentials & show validation status message

--- src/script.js
+++ src/script.js
@@ -24,10 +24,15 @@ addEventListener("submit", (event) => {
 
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
-  /*
-    TODO:
-    1. Check if the email and password are valid (using the usersTable)
-    2. If they are, call renderSuccess()
-    3. If they are not, call renderError()
-   */
+
+  let valid = false;
+
+  for (user of usersTable) {
+    if (email === user.username && password === user.password) {
+      valid = true;
+      break;
+    } else valid = false;
+  }
+
+  valid ? renderSuccess() : renderError();
 });
