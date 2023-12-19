adac8b187978b5b46c4ece7963fcef3fac7dfd26
Daniel Abella <djabella300@gmail.com>
2023-12-19 18:42:19 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -25,6 +25,18 @@ document.getElementById("show-password").addEventListener("change", function () 
   }
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+  // Your JavaScript code that interacts with the DOM goes here
+
+  // For example, you can access the 'embed' element here
+  var embedElement = document.querySelector("embed");
+  if (embedElement) {
+    var src = embedElement.getAttribute("src");
+    console.log("Embed element src:", src);
+  }
+});
+
+
 addEventListener("submit", (event) => {
   event.preventDefault();
   resetMessage();
