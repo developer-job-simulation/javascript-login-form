0da7bb831ed757b00547b945c77ec0cdde6b027d
willson-alflen <willson.alflen@gmail.com>
2024-03-17 19:59:26 -0300

    fix all issues

--- src/script.js
+++ src/script.js
@@ -1,33 +1,46 @@
 const usersTable = [
   // Note: This is a fake table for educational purposes. Never store user credentials in plain text.
-  { id: 1, username: "hello@world.com", password: "badpassword" },
-  { id: 2, username: "test@user.com", password: "badpassword" },
-  { id: 3, username: "email@domain.com", password: "badpassword" },
-];
+  { id: 1, username: 'hello@world.com', password: 'badpassword' },
+  { id: 2, username: 'test@user.com', password: 'badpassword' },
+  { id: 3, username: 'email@domain.com', password: 'badpassword' },
+]
 let renderSuccess = () => {
-  document.getElementById("success-message").hidden = false;
-};
+  document.getElementById('success-message').hidden = false
+}
 let renderError = () => {
-  document.getElementById("error-message").hidden = false;
-};
+  document.getElementById('error-message').hidden = false
+}
 let resetMessage = () => {
-  document.getElementById("success-message").hidden = true;
-  document.getElementById("error-message").hidden = true;
-};
+  document.getElementById('success-message').hidden = true
+  document.getElementById('error-message').hidden = true
+}
 
-addEventListener("submit", (event) => {
-  event.preventDefault();
-  resetMessage();
+function findUser(username, password) {
+  return usersTable.find(
+    (user) => user.username === username && user.password === password
+  )
+}
 
-  let email = document.getElementById("email").value;
-  let password = document.getElementById("password").value;
+addEventListener('submit', (event) => {
+  event.preventDefault()
+  resetMessage()
 
-  console.log(`email submitted: ${email}`);
-  console.log(`password submitted: ${password}`);
+  let email = document.getElementById('email').value
+  let password = document.getElementById('password').value
+
+  console.log(`email submitted: ${email}`)
+  console.log(`password submitted: ${password}`)
   /*
     TODO:
     1. Check if the email and password are valid (using the usersTable)
     2. If they are, call renderSuccess()
     3. If they are not, call renderError()
    */
-});
+  let user = findUser(email, password)
+
+  if (user) {
+    renderSuccess()
+  } else {
+    renderError()
+  }
+})
