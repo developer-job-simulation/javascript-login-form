028e4fe100e559d951697784aa14dc04241bb9d2
Vavrinecz Péter <vavrinecz.peter@gmail.com>
2025-02-06 17:45:08 +0100

    user validation & status message

--- src/script.js
+++ src/script.js
@@ -24,10 +24,11 @@ addEventListener("submit", (event) => {
 
   console.log(`email submitted: ${email}`);
   console.log(`password submitted: ${password}`);
-  /*
-    TODO:
-    1. Check if the email and password are valid (using the usersTable)
-    2. If they are, call renderSuccess()
-    3. If they are not, call renderError()
-   */
+  
+  user = usersTable.find(user => user.username === email && user.password === password)
+  if (user) {
+    renderSuccess()
+  } else {
+    renderError()
+  }
 });
